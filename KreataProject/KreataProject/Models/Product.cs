03c9d6452efd3a1403fc30a086667bca36f202a5
szt3rackev <szt3rackev@SZT5-9-HP>
2024-11-20 15:13:26 +0100

    FélKész

---
+++ KreataProject/KreataProject/Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KreataProject.Models
+{
+    public class Product
+    {
+        public Product(Guid id, string name, string csalad, string datumLejarat, int price, string oregsegiSzint)
+        {
+            Id = id;
+            string Name = name;
+            string Csalad = csalad;
+            string DatumLejarat = datumLejarat;
+            int Price = price;
+            OregsegiSzint = oregsegiSzint;  
+        }
+
+        public Product(string name, string csalad, string datumLejarat, int price, string oregsegiSzint)
+        {
+            Id = new Guid();
+            string Name = name;
+            string Csalad = csalad;
+            string DatumLejarat = datumLejarat;
+            int Price = price;
+            string OregsegiSzint = oregsegiSzint;
+        }
+
+        public Product()
+        {
+            Id = new Guid();
+            Name = string.Empty;
+            Csalad = string.Empty;
+            DatumLejarat = string.Empty;
+            Price = 0;
+            OregsegiSzint = string.Empty;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Csalad { get; set; }
+        public string DatumLejarat { get; set; }
+        public int Price { get; set; }
+        public string OregsegiSzint { get; set; }
+        public bool HasId => Id != Guid.Empty;
+
+
+        public override string ToString()
+        {
+            return $"{Name} {Csalad}, szavatossági lejárat: {DatumLejarat}, ára: {Price}, értéke: {OregsegiSzint}";
+        }
+    }
+}
---
+++ KreataProject/KreataProject/ViewModels/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using KreataProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KreataProject.ViewModels
+{
+    public partial class ProductViewModel : ObservableObject
+    {
+        [ObservableProperty]
+
+        private Product _product = new Product();
+
+        [ObservableProperty]
+        private List<string> _etelcsalad = new List<string> { "új", "épp", "öreg" };
+
+
+    }
+}
